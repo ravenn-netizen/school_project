b6e4ae455e940fe0670b32e9cc9cb076f0a5679e
aakshaaya <wowaks108@gmail.com>
2025-10-09 22:01:04 +0300

    Update sqlconnection.py

--- sqlconnection.py
+++ sqlconnection.py
@@ -6,20 +6,20 @@ cursor = db.cursor()
 
 #to create database
 def createdb():
-  cmd= "CREATE DATABASE SCHOOL"
-  cursor.execute(cmd)
-  cmd= "USE SCHOOL"
-  cursor.execute(cmd)
+    cmd= "CREATE DATABASE SCHOOL"
+    cursor.execute(cmd)
+    cmd= "USE SCHOOL"
+    cursor.execute(cmd)
 
 #to create table 'student'
 def createTableStudent():
-  cmd="CREATE TABLE STUDENT(grno int primary key, name )"
-  cursor.execute(cmd)
+    cmd="CREATE TABLE STUDENT(grno int primary key, name )"
+    cursor.execute(cmd)
 
 #to create table 'academic'
 def createTableAcademic():
-  cmd="CREATE TABLE ACADEMIC()"
-  cursor.execute(cmd)
+    cmd = "CREATE TABLE ACADEMIC(GRNO INT PRIMARY KEY, SUB1 FLOAT, SUB2 FLOAT, SUB3 FLOAT, SUB4 FLOAT, SUB5 FLOAT, AVG FLOAT, REMARKS"
+    cursor.execute(cmd)
 
 
 
