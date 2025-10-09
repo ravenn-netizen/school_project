c5ea240b1cf6fecb016f7aa4f3c817ef92eabe05
akshaya :) <wowaks108@gmail.com>
2025-10-09 20:50:11 +0300

    Update and rename database.py to sqlconnection.py

---
+++ sqlconnection.py
@@ -0,0 +1,25 @@
+#to establish python-sql connection
+
+import mysqlconnector as sql
+db = sql.conect(host='localhost', user='root', passwd='****')
+cursor = db.cursor()
+
+#to create database
+def createdb():
+  cmd= "CREATE DATABASE SCHOOL"
+  cursor.execute(com)
+  cmd= "USE SCHOOL"
+  cursor.execute(com)
+
+#to create table 'student'
+def createTableStudent():
+  cmd="CREATE TABLE STUDENT(grno int primary key, name )"
+  cursor.execute(cmd)
+
+#to create table 'academic'
+def createTableAcademic():
+  cmd="CREATE TABLE ACADEMIC()"
+  cursor.execute(cmd)
+
+
+
