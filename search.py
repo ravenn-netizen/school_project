5d01f823286f849ede459473bd418c2d24d283b0
sneeehaaa <snehasusanthomas9@gmail.com>
2025-10-11 11:29:18 +0300

    Update and rename search to search.py

---
+++ search.py
@@ -0,0 +1,52 @@
+import mysql.connector as sql
+db=sql.connect(host='localhost',user='root',password='1922',database='School')
+cursor=db.cursor()
+def search_grno():
+  Gr=int(input("enter grno"))
+  query="select*from student where grno={}".format(Gr)
+  cursor.execute(query)
+  rec=cursor.fetchone()
+  if rec:
+    print("Grnno="rec[0],"class="rec[1],"name="rec[2],"stream="rec[3],"house"=[4])
+  else:
+    print("no record found")
+def search_class():
+  Class=input("enter class")
+  query="select*from student where class='{}'".format(Class)
+  cursor.execute(query)
+  rec=cursor.fetchall()
+  if rec:
+    print("Grnno="rec[0],"class="rec[1],"name="rec[2],"stream="rec[3],"house"=[4])
+  else:
+    print("no record found")
+def search_name():
+  Name=input("enter name")
+  query="select*from student where name='{}'".format(Name)
+  cursor.execute(query)
+  rec=cursor.fetchall()
+  if rec:
+    print("Grnno="rec[0],"class="rec[1],"name="rec[2],"stream="rec[3],"house"=[4])
+  else:
+    print("no record found")
+def search_stream():
+  Stream=input("enter stream")
+  query="select*from student where stream='{}'".format(Stream)
+  cursor.execute(query)
+  rec=cursor.fetchall()
+  if rec:
+    print("Grnno="rec[0],"class="rec[1],"name="rec[2],"stream="rec[3],"house"=[4])
+  else:
+    print("no record found")
+def search_house():
+  House=input("enter house")
+  query="select*from student where house='{}'".format(House)
+  cursor.execute(query)
+  rec=cursor.fetchall()
+  if rec:
+    print("Grnno="rec[0],"class="rec[1],"name="rec[2],"stream="rec[3],"house"=[4])
+  else:
+    print("no record found")
+  
+
+
+
