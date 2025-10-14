fcc18458fc97607d0d8706aed3d61a273d07f951
aakshaaya <wowaks108@gmail.com>
2025-10-14 12:32:43 +0300

    Update database.py

--- sqlconnection.py
+++ sqlconnection.py
@@ -11,15 +11,17 @@ def createdb():
     cmd= "USE SCHOOL"
     cursor.execute(cmd)
 
-#to create table 'student'
+#to create table 'STUDENT'
 def createTableStudent():
-    cmd="CREATE TABLE STUDENT(GRNO int primary key, NAME )"
+    cmd="CREATE TABLE STUDENT(STUDENTID INT PRIMARY KEY, NAME VARCHAR(30), GENDER CHAR(1), CLASS INT, SECTION CHAR(1), STREAM VARCHAR(10), TRANSPORT VARCHAR(10), BUS_NO INT, GUARDIAN VARCHAR(40), TEL INT, EMAIL VARCHAR(20), PO_BOX INT, ADDRESS DICT, CPR INT)"
     cursor.execute(cmd)
 
-#to create table 'academic'
+#to create table 'ACADEMIC'
 def createTableAcademic():
-    cmd = "CREATE TABLE ACADEMIC(GRNO INT PRIMARY KEY, SUB1 FLOAT, SUB2 FLOAT, SUB3 FLOAT, SUB4 FLOAT, SUB5 FLOAT, AVG FLOAT, REMARKS varchar(10)"
+    cmd = "CREATE TABLE ACADEMIC(STUDENTID INT PRIMARY KEY, SUB1 FLOAT, SUB2 FLOAT, SUB3 FLOAT, SUB4 FLOAT, SUB5 FLOAT, AVG FLOAT, REMARKS varchar(10) '''pass or fail'''"
     cursor.execute(cmd)
 
-
-
+#to create table 'STAFF'
+def createTableStaff():
+    cmd = "CREATE TABLE STAFF(STAFFID INT PRIMARY KEY, NAME VARCHAR(30), DEPARTMENT VARCHAR(20), CPR INT DISTINCT)
+    cursor.execute(cmd)
