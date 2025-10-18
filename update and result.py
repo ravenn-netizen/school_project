05b1427c57ba46f24a5d275bf993fafa2aa688e6
shan-epsiba <epsibadaniel1922@gmail.com>
2025-10-18 20:04:58 +0300

    Update update and result.py

--- update and result.py
+++ update and result.py
@@ -1,10 +1,10 @@
 import mysql.connector as sql
 db=sql.connect(host='localhost',user='root',password='1922',database='School')
 cursor=db.cursor()
-gr=int(input("enter gr.no of the student to be updated:"))
 
 #update class 
 def updateclass():
+    gr=int(input("enter gr.no of the student to be updated:"))
     nclass=int(input("Enter new class of the student:"))
     com="update student set Class='{}' where Grno={}".format(nclass,gr)
     cursor.execute(com)
@@ -12,6 +12,7 @@ def updateclass():
 
 #update bus stop
 def updatebusstop():
+    gr=int(input("enter gr.no of the student to be updated:"))
     nbusstop=input("Enter new bus stop of the student:")
     com="update student set Busstop='{}' where Grno={}".format(nbusstop,gr)
     cursor.execute(com)
@@ -19,6 +20,7 @@ def updatebusstop():
 
 #update bus no
 def updatebusno():
+    gr=int(input("enter gr.no of the student to be updated:"))
     nbusno=int(input("Enter new bus number:"))
     com="update student set Busno={} where Grno={}".format(nbusno,gr)
     cursor.execute(com)
@@ -26,6 +28,7 @@ def updatebusno():
     
 #update telephone no
 def updatetel():
+    gr=int(input("enter gr.no of the student to be updated:"))
     nph=input("Enter new phone number of the student:")
     com="update student set Telephoneno='{}' where Grno={}".format(nph,gr)
     cursor.execute(com)
@@ -50,6 +53,8 @@ while True:
         break
 
 #update staff passwd
+def updatestaffpass():
+    
         
 
 
@@ -57,6 +62,7 @@ while True:
             
                     
         
+
 
 
 
