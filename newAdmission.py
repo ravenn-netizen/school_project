9e45aa502a9a53664f355b987c72add660c4b764
aakshaaya <wowaks108@gmail.com>
2025-10-25 17:54:25 +0300

    Update newAdmission.py

--- newAdmission.py
+++ newAdmission.py
@@ -1,29 +1,35 @@
-def newAdm():
+def newAdmission():
+    name = input("enter name: ")
+    dob = input('enter date of birth YYYY-MM-DD: ')
+    gender = input('enter gender (M or F): ')
+    age = int(input('Enter age: '))
+    clss = int(input("enter class (11-12): "))
+    section = input("enter section (A-Z): ")
+    stream = 
+    transport = input("enter transport (bus or private): ")
 
-    #user input values
-    cpr = int(input('enter cpr number: '))
-    name = input('enter name: ').strip()
-    dob = input('enter dob YYYY-MM-DD: ').strip()
-    stream = input('enter stream (sci, com, hum): ').strip
-    clss = int(input('enter class (11-12): '))
-    secn = input('enter section (A-Z): ').strip()
-    tel = int(input('enter telephone number: '))
-    pname = input('enter parent\'s name (each seperated by \' & \': ').partition(' & ')
+    if transport == 'bus':
+        bus_no  = int(input("enter bus number: "))
+        bus_stop = input("enter bus stop: ")
+    else:
+        bus_no , bus_stop  = None, None 
+
+    guardian = input("enter name of guardian(s) (each separated by a \' & \'): ")
+    tel = input('enter phone number: ')
+    email = input('enter email: ')
+    address = 
+    cpr = input('enter student\'s cpr number: ')
+
+    #to generate new student_id
+    cmd = "SELECT MAX(STUDENT_ID) FROM STUDENT"
+    cursor.execute(cmd)
+    student_id = cursor.fetchone() + 1
+
+    cmd = "INSERT INTO STUDENT VALUES('{}', '{}', '{}', '{}', {}, {}, '{}', {}, '{}', {}, '{}', '{}', '{}', '{}', '{}', '{}' )",format(student_id, name, dob, gender, age, clss, section, stream, transport, bus_no, bus_stop, guardian, tel, email, address,cpr)
+    cursor.execute(cmd)
+    db.commit()
     
-    address = {'flat':0, 'bldg':0, 'road':0, 'block':0, 'area':0}
-    print('enter address;')
-    for k in address:
-        address[k] = int(input('enter {}: '.format(k)))
 
-    #to generate new grno by finding the maximum existing
-    cmd = "SELECT MAX(GRNO) FROM STUDENT"
-    cursor.execute(cmd) 
-    maxgr = cursor.fetchone()
-    gr = maxgr +1 
-
-    #to insert
-    cmd = "INSERT INTO STUDENT VALUES()"
-    
 
 
 
