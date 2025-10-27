820bdea44d1e0118cbabaee6e12d728e87bbb6b9
aakshaaya <wowaks108@gmail.com>
2025-10-27 21:10:53 +0300

    Update report.py

--- report.py
+++ report.py
@@ -7,9 +7,8 @@ def report():
        
         
     if opt == 1:
-
-        query = "SELECT S.GRADE, S.SECTION, COUNT(A.STUDENT_ID), avg(A.AVG) FROM STUDENT S NATURAL JOIN ACADEMIC A GROUP BY GRADE, SECTION ORDER BY GRADE, SECTION".format(grade, section)
-        cursor.execute(query)
+        grade = int(input('enter grade: ')
+        query = "SELECT S.GRADE, S.SECTION, COUNT(A.STUDENT_ID), avg(A.AVG) FROM STUDENT S NATURAL JOIN ACADEMIC WHERE GRADE = {} A GROUP BYSECTION ORDER BYSECTION".format(grade)
         records = cursor.fetchall()
         for i in records:
             for k in i:
