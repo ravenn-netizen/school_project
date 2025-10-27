ad9e2d325db25395a9e9b3264d520f24aef1e1e7
aakshaaya <wowaks108@gmail.com>
2025-10-27 21:07:35 +0300

    Update report.py

--- report.py
+++ report.py
@@ -8,7 +8,7 @@ def report():
         
     if opt == 1:
 
-        query = "SELECT S.GRADE, S.SECTION, COUNT(A.STUDENT_ID), avg(A.AVG) FROM STUDENT S NATURAL JOIN ACADEMIC A GROUP BY GRADE, SECTION".format(grade, section)
+        query = "SELECT S.GRADE, S.SECTION, COUNT(A.STUDENT_ID), avg(A.AVG) FROM STUDENT S NATURAL JOIN ACADEMIC A GROUP BY GRADE, SECTION ORDER BY GRADE, SECTION".format(grade, section)
         cursor.execute(query)
         records = cursor.fetchall()
         for i in records:
@@ -21,7 +21,7 @@ def report():
         grade = int(input('enter grade: '))
         section = input("enter section: ")
 
-        query = "SELECT NAME, STUDENT.STUDENT_ID, SUB1, SUB2, SUB3, SUB4, SUB5, AVG, REMARK FROM STUDENT NATURAL JOIN ACADEMIC WHERE GRADE={} AND SECTION={}".format(grade, section)
+        query = "SELECT NAME, S.STUDENT_ID, A.SUB1, A.SUB2, A.SUB3, A.SUB4, A.SUB5, A.AVG, A.REMARK FROM STUDENT S NATURAL JOIN ACADEMIC A WHERE GRADE={} AND SECTION={} ORDER BY NAME".format(grade, section)
         cursor.execute(query)
         records = cursor.fetchall()
         for i in records:
@@ -29,4 +29,10 @@ def report():
                 print(k, end='\t')
             print()
 
-        query = "SELECT COUNT(S.STUDENT_ID), MIN(A.AVG), MAX(A.AVG), AVG(A.AVERAGE) FROM STUDENT S NATURAL JOIN ACADEMIC A "
+        query = "SELECT COUNT(S.STUDENT_ID) AS TOTAL STUDENTS, MIN(A.AVG) AS MINIMUM_AVERAGE, MAX(A.AVG) AS MAXIMUM_AVERAGE, AVG(A.AVERAGE) AS CLASS_AVERAGE FROM STUDENT S NATURAL JOIN ACADEMIC A "
+        cursor.execute(query)
+        records = cursor.fetchall()
+        for i in records:
+            for k in i:
+                print(k, end='\t')
+            print()        
