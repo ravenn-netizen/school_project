f4d4376d2da4693db0e3d69ea22e19481108126e
aakshaaya <wowaks108@gmail.com>
2025-11-01 20:32:15 +0300

    Update sign_up&sign_in.py

--- sign_up&sign_in.py
+++ sign_up&sign_in.py
@@ -12,7 +12,7 @@ def staff_sign_up():
     query = 'SELECT max(STAFF_ID) FROM STAFF'
     cursor.execute(query)
     result = cursor.fetchone()
-    if result is None: #this is in case the table is empty and there are no values for staffid, in that case im creating a new one ie 'T0'
+    if result[0] is None: #this is in case the table is empty and there are no values for staffid, in that case im creating a new one ie 'T0'
         staff_id = 'T0'
     else:
         #this is in case there are already some records inthe table
@@ -23,20 +23,23 @@ def staff_sign_up():
         staff_id = 'T'+ str(int(result[0][1:]) + 1)
 
     #to insert details into table STAFF
-    cmd= "INSERT INTO STAFF VALUES({}, '{}', '{}', {}, '{}', '{}')".format(staff_id, name, dept, cpr, password)
+    cmd= "INSERT INTO STAFF VALUES('{}', '{}', '{}','{}', '{}')".format(staff_id, name, dept, cpr, password)
     cursor.execute(cmd)
     db.commit()
 
 def staff_sign_in():
     # to enter staff id and check if in database
     while True:
-        staff_id = int(input('enter staff id: '))
-        cmd = 'SELECT STAFFID FROM STAFF'
+        staff_id =input('enter staff id: ')
+        cmd = 'SELECT STAFF_ID FROM STAFF'
         cursor.execute(cmd)
         records = cursor.fetchall()
-        if tuple(staffID) in records:
+
+        if (staff_id,) in records:
+            print(records)
             break
         else:
+            print(records)
             print('staff id entered does not exist')
             print('1. try again')
             print('2. create a new account')
@@ -50,34 +53,36 @@ def staff_sign_in():
 
     password = input('enter password: ')
     
-    cmd = 'SELECT STAFFID, PASSWORD FROM STAFF'
-    cursor.execute()
+    cmd = 'SELECT STAFF_ID, PASSWORD FROM STAFF'
+    cursor.execute(cmd)
     records = cursor.fetchall()
     while True:
         for record in records:
             if record[0] == staff_id and record[1] == password:
                 break 
-            else: 
-                print('wrong password')
-                password = int(input('enter password: '))
+        else: 
+            print('wrong password')
+            password = input('enter password: ')
+    print('welcome to the database :)')
 
 
-    user_id = staff_id
+    
     global user_id
+    user_id = staff_id
     global user_type 
     user_type = 'staff'
     
     print('welcome to the database :)')
 
 def student_sign_in():
-    cursor.execute('USE STUDENT')
+    cursor.execute('USE SCHOOL')
     
     while True:
-        student_id = int(input('enter student id: '))
+        student_id = input('enter student id: ')
         cmd = 'SELECT STUDENT_ID FROM STUDENT'
         cursor.execute(cmd)
         records = cursor.fetchall()
-        if tuple(studentID) in records:
+        if (student_id,) in records:
             break
         else:
             print('student id entered does not exist')
@@ -87,33 +92,21 @@ def student_sign_in():
     password = input('enter password: ')
     
     cmd = 'SELECT STUDENT_ID, PASSWORD FROM STUDENT'
-    cursor.execute()
+    cursor.execute(cmd)
     records = cursor.fetchall()
     while True:
         for record in records:
-            if record[0] == student_id and record[1] == passwd:
+            if record[0] == student_id and record[1] == password:
                 break 
-            else: 
+        else: 
                 print('wrong password OR password doesnt match user')
-                passwd = int(input('enter password: '))
+                password =input('enter password: ')
 
-    user_id = studentID
+    print('welcome to the database :)')   
 
     global user_id
+    user_id = student_id
     global user_type 
     user_type = 'student'
     
     print('welcome to the database :)')
-
-
-
-
-
-
-
-
-
-
-
-
-
