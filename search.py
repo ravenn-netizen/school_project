e8adb07304fc2233e8be28761df60e0176c3120d
sneeehaaa <snehasusanthomas9@gmail.com>
2025-11-13 22:52:10 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,44 +1,49 @@
+from tabulate import tabulate
 import mysql.connector as sql
-db=sql.connect(host='localhost',user='root',password='1922',database='School')
-cursor=db.cursor()
+db = sql.connect(host='localhost', user='root', password='1922', database='School')
+cursor = db.cursor()
 def search_STUDENTID():
-  id=int(input("enter STUDENT_ID"))
-  query="select*from student where grno={}".format(id)
-  cursor.execute(query)
-  rec=cursor.fetchone()
-  if rec:
-    print("STUDENT_ID=",r[0], "NAME=",r[1], "DOB=",r[2], "GENDER=",r[3], "AGE=",r[4], "GRADE=",r[5], "SECTION=",r[6], "STREAM=",r[7], "TRANSPORT=",r[8], "BUS.NO=",r[9], "BUS STOP=",r[10], "GUARDIAN=",r[11], "TEL=",r[12], "EMAIL=",r[13], "ADDRESS=",r[14], "CPR=",r[15])
-  else:
-    print("no record found")
+    id = int(input("Enter STUDENT_ID: "))
+    query = "SELECT * FROM student WHERE STUDENT_ID = {}".format(id)
+    cursor.execute(query)
+    rec = cursor.fetchone()
+    if rec:
+        headers = ["STUDENT_ID","NAME","DOB","GENDER","AGE","GRADE","SECTION","STREAM",
+                   "TRANSPORT","BUS.NO","BUS STOP","GUARDIAN","TEL","EMAIL","ADDRESS","CPR"]
+        table = [rec]
+        print(tabulate(table, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("No record found")
 def search_class():
-  Class=input("enter class")
-  query="select*from student where class='{}'".format(Class)
-  cursor.execute(query)
-  rec=cursor.fetchall()
-  if rec:
-    for r in rec:
-      print("STUDENT_ID=",r[0], "NAME=",r[1], "DOB=",r[2], "GENDER=",r[3], "AGE=",r[4], "GRADE=",r[5], "SECTION=",r[6], "STREAM=",r[7], "TRANSPORT=",r[8], "BUS.NO=",r[9], "BUS STOP=",r[10], "GUARDIAN=",r[11], "TEL=",r[12], "EMAIL=",r[13], "ADDRESS=",r[14], "CPR=",r[15])
-  else:
-    print("no record found")
+    Class = input("Enter class: ")
+    query = "SELECT * FROM student WHERE class = '{}'".format(Class)
+    cursor.execute(query)
+    rec = cursor.fetchall()
+    if rec:
+        headers = ["STUDENT_ID","NAME","DOB","GENDER","AGE","GRADE","SECTION","STREAM",
+                   "TRANSPORT","BUS.NO","BUS STOP","GUARDIAN","TEL","EMAIL","ADDRESS","CPR"]
+        print(tabulate(rec, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("No record found")
 def search_name():
-  Name=input("enter name")
-  query="select*from student where name='{}'".format(Name)
-  cursor.execute(query)
-  rec=cursor.fetchall()
-  if rec:
-    for r in rec:
-      print("STUDENT_ID=",r[0], "NAME=",r[1], "DOB=",r[2], "GENDER=",r[3], "AGE=",r[4], "GRADE=",r[5], "SECTION=",r[6], "STREAM=",r[7], "TRANSPORT=",r[8], "BUS.NO=",r[9], "BUS STOP=",r[10], "GUARDIAN=",r[11], "TEL=",r[12], "EMAIL=",r[13], "ADDRESS=",r[14], "CPR=",r[15])
-  else:
-    print("no record found")
+    Name = input("Enter name: ")
+    query = "SELECT * FROM student WHERE name = '{}'".format(Name)
+    cursor.execute(query)
+    rec = cursor.fetchall()
+    if rec:
+        headers = ["STUDENT_ID","NAME","DOB","GENDER","AGE","GRADE","SECTION","STREAM",
+                   "TRANSPORT","BUS.NO","BUS STOP","GUARDIAN","TEL","EMAIL","ADDRESS","CPR"]
+        print(tabulate(rec, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("No record found")
 def search_stream():
-  Stream=input("enter stream")
-  query="select*from student where stream='{}'".format(Stream)
-  cursor.execute(query)
-  rec=cursor.fetchall()
-  if rec:
-    for r in rec:
-      print("STUDENT_ID=",r[0], "NAME=",r[1], "DOB=",r[2], "GENDER=",r[3], "AGE=",r[4], "GRADE=",r[5], "SECTION=",r[6], "STREAM=",r[7], "TRANSPORT=",r[8], "BUS.NO=",r[9], "BUS STOP=",r[10], "GUARDIAN=",r[11], "TEL=",r[12], "EMAIL=",r[13], "ADDRESS=",r[14], "CPR=",r[15])
-  else:
-    print("no record found")
-
-
+    Stream = input("Enter stream: ")
+    query = "SELECT * FROM student WHERE stream = '{}'".format(Stream)
+    cursor.execute(query)
+    rec = cursor.fetchall()
+    if rec:
+        headers = ["STUDENT_ID","NAME","DOB","GENDER","AGE","GRADE","SECTION","STREAM",
+                   "TRANSPORT","BUS.NO","BUS STOP","GUARDIAN","TEL","EMAIL","ADDRESS","CPR"]
+        print(tabulate(rec, headers=headers, tablefmt="fancy_grid"))
+    else:
+        print("No record found")
