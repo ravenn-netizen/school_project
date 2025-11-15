164cb5b13ea08b75d097a329262dc864c37fad30
aakshaaya <wowaks108@gmail.com>
2025-11-15 23:13:40 +0300

    Update sign_in.py

--- sign_in.py
+++ sign_in.py
@@ -1,9 +1,5 @@
-#complete
-#under testing
-
 def staff_sign_in():
     # to enter staff id and check if in database
-    cursor.execute('USE SCHOOL')
     
     while True:
         staff_id =int(input('enter staff id: '))
@@ -15,9 +11,9 @@ def staff_sign_in():
             break
         else:
             print(records)
-            print('staff id entered does not exist')
+            print('\nstaff id entered does not exist')
             print('1. try again')
-            print('2. exit to main menu')
+            print('2. exit to main menu\n')
             opt = int(input('enter option: '))
 
             if opt == 1:
@@ -32,25 +28,20 @@ def staff_sign_in():
     while True:
         entered_password = input('enter password: ')
         if entered_password == password_in_db:
+            print("Hello, staff '{}'".format(staff_id))
+            print("Welcome to the database.\n")
             return staff_id
         else: 
-            print('wrong password')
+            print('\nwrong password')
             print('1. try password again')
-            print('2. exit to main menu')
+            print('2. exit to main menu\n')
             opt = int(input('enter option: '))
             if opt == 1:
                 continue
             if opt == 2:
                 return 0
     
-    print('welcome to the database :)')
-#also include the below three lines of code when using the function          
-user_id = staff_sign_in()
-if user_id == 0:
-    continue
-
 def student_sign_in():
-    cursor.execute('USE SCHOOL')
     
     while True:
         student_id = int(input('enter student id: '))
@@ -60,9 +51,9 @@ def student_sign_in():
         if (student_id,) in records:
             break
         else:
-            print('student id entered does not exist')
+            print('\nstudent id entered does not exist')
             print('1. try again')
-            print('2. exit to main menu')
+            print('2. exit to main menu\n')
             opt = int(input('enter option: '))
 
             if opt == 1:
@@ -72,29 +63,24 @@ def student_sign_in():
     
     cmd = "SELECT PASSWORD FROM STUDENT WHERE STUDENT_ID = '{}'".format(student_id)
     cursor.execute(cmd)
-    password_in_db = cursor.fetchall()[0]
+    password_in_db = cursor.fetchone()[0]
 
     while True: 
         entered_password = input('enter password: ')
         if entered_password == password_in_db:
+            print('Hello, student "{}"!'.format(student_id))
+            print('Welcome to the database.\n')
             return student_id
         else: 
-            print('wrong password')
+            print('\nwrong password')
             print('1. try password again')
-            print('2. exit to main menu')
+            print('2. exit to main menu\n')
             opt = int(input('enter option: '))
             if opt == 1:
                 continue
             if opt == 2:
                 return 0
 
-    print('welcome to the database :)')  
-    
-#also include the below three lines of code when using the function          
-user_id = student_sign_in()
-if user_id == 0:
-    continue
-    
 def admin_sign_in():
     set_password = "admin's password"
 
@@ -111,31 +97,3 @@ def admin_sign_in():
                 continue
             else:
                 return 0 #user is not signed in and gets returned to main menu #refer menu() for reference
-                      
-def admin_menu():
-    print("--- MENU ---")
-    print("1. staff management")
-    print("2. student management")
-
-    opt = int(input("enter option: "))
-
-    if opt == 1:
-        new_staff()
-
-    if opt == 2:
-        print("1. new student")
-        print("2. search student")
-        print("3. remove student")
-        choice = int(input("Enter choice :"))
-        if choice == 1:
-            new_student()
-        elif choice == 2:
-            searchSTUDENTID()
-        elif choice == 3:
-            delstudent()
-
-
-
-
-
-
